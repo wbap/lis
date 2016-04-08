555bbf47766832243e4f0a9b5be8a6b201a536d4
Masayoshi Nakamura <mchmasa@gmail.com>
2016-04-08 22:49:25 +0900

    modify fetch.sh  wget -> curl

--- fetch.sh
+++ fetch.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 echo "download caffemodel..."
-wget -O python-agent/bvlc_alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
+curl -o python-agent/bvlc_alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
 
-wget -O python-agent/ilsvrc_2012_mean.npy https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
+curl -f -L -o python-agent/ilsvrc_2012_mean.npy https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
 
