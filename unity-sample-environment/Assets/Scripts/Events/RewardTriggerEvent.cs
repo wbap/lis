fbf9617a3702040acc14b8428ba6965498019276
Masayoshi Nakamura <mchmasa@gmail.com>
2016-04-22 19:43:14 +0900

    add RewardArea

---
+++ unity-sample-environment/Assets/Scripts/Events/RewardTriggerEvent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MLPlayer {
+	public class RewardTriggerEvent : MonoBehaviour {
+		[SerializeField] float rewardOnTriggerEnter;
+		[SerializeField] float rewardOnTriggerExit;
+		[SerializeField] float rewardOnTriggerStayPerFixedDeltaTime;
+
+		bool IsPlayer(GameObject obj) {
+			return obj.tag == Defs.PLAYER_TAG;
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if (IsPlayer(other.gameObject)) {
+				other.gameObject.GetComponent<Agent> ().AddReward (rewardOnTriggerEnter);
+				//Debug.Log ("OnTriggerEnter");
+			}
+		}
+
+		void OnTriggerExit(Collider other) {
+			if (IsPlayer(other.gameObject)) {
+				other.gameObject.GetComponent<Agent> ().AddReward (rewardOnTriggerExit);
+				//Debug.Log ("OnTriggerExit");
+			}
+		}
+
+		void OnTriggerStay(Collider other) {
+			if (IsPlayer(other.gameObject)) {
+				other.gameObject.GetComponent<Agent> ().AddReward (rewardOnTriggerStayPerFixedDeltaTime);
+				//Debug.Log ("OnTriggerStay:");
+			}
+		}
+	}
+}
